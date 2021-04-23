c3df68d2628273c32b2087ccc91cd7c9e0db5b00
Beerthouille <82842028+Beerthouille@users.noreply.github.com>
2021-04-24 01:04:51 +0200

    French Translation

--- Localization/Translations/TrackerUI.lua
+++ Localization/Translations/TrackerUI.lua
@@ -144,7 +144,7 @@ local trackerUILocales = {
         ["zhCN"] = "将任务连接到聊天框",
         ["zhTW"] = "將任務連結貼到對話",
         ["esES"] = "Vincular misión al chat",
-        ["frFR"] = nil,
+        ["frFR"] = "Insérer la quête dans le chat",
     },
     ["Show in Quest Log"] = {
         ["ptBR"] = "Visualizar as missões",
